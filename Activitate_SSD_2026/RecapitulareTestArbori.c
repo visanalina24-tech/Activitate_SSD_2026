934e41171e4ce330bd30e7d1e5ddd65cfafd4c7b
Vișan Alina <visan.alina2005@gmail.com>
2026-05-17 17:25:40 +0300

    recapitulare arbori binari

---
+++ Activitate_SSD_2026/RecapitulareTestArbori.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Masina Masina;
+typedef struct Nod Nod;
+
+struct Masina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* marca;
+	unsigned char serie;
+};
+struct Nod {
+	Masina masina;
+    Nod* next;
+	Nod* prev;
+};
+void afisareMasina(Masina masina) {
+	printf("ID: %d\n", masina.id);
+	printf("Nr. usi: %d\n", masina.nrUsi);
+	printf("Pret: %f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Marca: %s\n", masina.marca);
+	printf("Serie: %c\n", masina.serie);
+}
+Masina citireMasinaDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = "\n,";
+	fgets(buffer, 100, file);
+	char* aux;
+	aux = strtok(buffer, sep);
+	Masina m;
+	m.id = atoi(strtok(NULL, sep));
+	m.nrUsi = atoi(strtok(NULL, sep));
+	m.pret = atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m.model = malloc(strlen(aux) + 1);
+	strcpy_s(m.model, strlen(aux) + 1, aux);
+	aux = strtok(NULL, sep);
+	m.marca = malloc(strlen(aux) + 1);
+	strpcy_s(m.marca, strlen(aux) + 1, aux);
+	m.serie = *strtok(NULL, sep);
+	return m;
+}
+void adaugaMasinaInArbore(Nod** arbore, Masina masinaNoua) {
+	if (*arbore == NULL) {
+		Nod* nou = (Nod*)malloc(sizeof(Nod));
+		nou->masina = masinaNoua;
+		nou->prev = NULL;
+		nou->next = NULL;
+		(*arbore) = nou;
+	}
+	else if ((*arbore)->masina.id > masinaNoua.id) {
+		adaugaMasinaInArbore(&((*arbore)->prev), masinaNoua);
+	}
+	else if ((*arbore)->masina.id <= masinaNoua.id) {
+		adaugaMasinaInArbore(&(*arbore)->next, masinaNoua);
+	}
+}
+Nod* citireArboreDeMasiniDinFisier(const char* numeFisier){
+	FILE* f = feopen(numeFisier, "r");
+	Nod* arbore = NULL;
+	while (!feof(f)) {
+		adaugaMasinaInArbore(&arbore, citireMasinaDinFisier(f));
+	}
+	fclose(f);
+	return arbore;
+}
+	
+
+
+
